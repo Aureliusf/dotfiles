af869009f5edc2beb041b59007b813bcf181c704
Aure <aure@Mac.hsd1.fl.comcast.net>
2025-05-25 01:10:53 -0400

    double mortal backflip into defaults

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -1,83 +1,42 @@
 {
-  description = "macbook config";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ 
-          pkgs.vim
-          pkgs.neovim
-          pkgs.direnv
-          pkgs.sshs
-          pkgs.glow
-          pkgs.nushell
-          pkgs.carapace
-          pkgs.tailscale
-          pkgs.neofetch
-          pkgs.fd
+        [ pkgs.neovim
         ];
-      services.nix-daemon.enable = true;
+
+      # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
-      programs.zsh.enable = true;  # default shell on catalina
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
-      system.stateVersion = 4;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
-      security.pam.enableSudoTouchIdAuth = true;
-
-      users.users.aure.home = "/Users/aure";
-      home-manager.backupFileExtension = "backup";
-      nix.configureBuildUsers = true;
-      nix.useDaemon = true;
-
-      system.defaults = {
-        dock.autohide = true;
-        dock.mru-spaces = false;
-        finder.AppleShowAllExtensions = true;
-        finder.FXPreferredViewStyle = "clmv";
-        loginwindow.LoginwindowText = "devops-toolbox";
-        screencapture.location = "~/Pictures/screenshots";
-        screensaver.askForPasswordDelay = 10;
-      };
-
-      # Homebrew needs to be installed on its own!
-      homebrew.enable = true;
-      homebrew.casks = [
-            "aerospace"
-            "qlzipinfo"
-            "trex"
-      ];
-      homebrew.brews = [
-	      "imagemagick"
-      ];
     };
   in
   {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
     darwinConfigurations."macbook" = nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      modules = [ 
-	configuration
-        home-manager.darwinModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.omerxx = import ./home.nix;
-        }
-      ];
+      modules = [ configuration ];
     };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."macbook".pkgs;
   };
 }
